22ce9c61bd7c255b73ffeddece6db7aebeadf12d
LanQin_ <88375967+LanQin996@users.noreply.github.com>
2025-11-07 15:10:08 +0800

    feat: add 网易云音乐API to navigation data for enhanced resource accessibility

--- docs/.vitepress/theme/untils/data.ts
+++ docs/.vitepress/theme/untils/data.ts
@@ -14,6 +14,16 @@ export const NAV_DATA: NavData[] = [
         desc: '在线图片链接,自带md格式,URL等 支持多图片格式上传',
         link: 'https://imgwwo.cn/'
       },
+      {
+        icon: 'https://cdn.jsdu.cn/opdav/20251107/076793d54a13c9dc04cfa7b3fb6580ad.webp',
+        title: '网易云音乐API',
+        badge: {
+          text: '免费公益',
+          type: 'tip',
+        },
+        desc: '网易云的API 可用于ZMusic插件或其他',
+        link: 'https://zm.wwoyun.cn/'
+      },
     ]
   },
 ]
